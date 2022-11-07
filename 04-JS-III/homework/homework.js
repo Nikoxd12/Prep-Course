b89d2a49364ce2b861a29d6f02ecfc44d6b0c546
Nikoxd12 <nikobarreto6@gmail.com>
2022-11-07 14:07:39 -0300

    En proceso

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -145,6 +145,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (diaDeLaSemana === 1 || diaDeLaSemana === 7) {
+    return "Es fin de semana";
+  }
+  else {
+    return "Es día laboral";
+  }
   
 } 
 
