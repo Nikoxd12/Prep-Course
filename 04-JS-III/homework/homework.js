e67463dc8e6b394dc99e3a70ddb0deabe04afd99
Nikoxd12 <nikobarreto6@gmail.com>
2022-11-08 23:17:05 -0300

    En proceso

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -159,14 +159,29 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let num = n.toString();
+  if (num.charAt(0) === 9) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+  }
   
-}
 
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+    for (var i = 0; i < arreglo.length - 1; i++) {
+      if (arreglo[i] !== arreglo[i]+1) {
+        return false;
+      }
+    }
+    return true;
+
   
 } 
 
@@ -176,6 +191,18 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var Nuevoarray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      Nuevoarray.push(array[i]);
+    }
+  } 
+  if (Nuevoarray < 3) {
+    return "No se encontraron los meses pedidos";
+  }
+  else {
+    return Nuevoarray;
+  }
 }
 
 
@@ -183,6 +210,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > 100) {
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 
